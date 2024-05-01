e40cc57dac4c751412662833ee22b93d3fb142e5
Hightree <166693367+hightree-dev@users.noreply.github.com>
2024-05-01 06:23:00 +0000

    10871

---
+++ source/10871.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int N, X, A;
+    std::cin >> N >> X;
+
+    for (int i = 0; i < N; ++i) {
+        std::cin >> A;
+        if (A < X)
+            std::cout << A << ' ';
+    }
+}
