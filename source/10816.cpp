95c7090bdae5ac60154ea9ee6e86d50cadc5fca1
Hightree <166693367+hightree-dev@users.noreply.github.com>
2024-05-08 22:44:26 +0900

    10816 v2
    이 문제를 풀면서 자료구조를 제대로 공부해야겠다는 생각이 들었다.
    단순히 map이 key value 구조인 줄 알았는데, key를 기반으로 알아서 정렬까지 해주더라... STL의 map은 레드 블랙 트리로 구현되어있어 insert를 하면 자동으로 정렬이 된다.

--- source/10816.cpp
+++ source/10816.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <map>
 #include <algorithm>
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    
     int N, num, count;
     std::cin >> N;
     
-    std::vector<int> v(N);
-    for(int i = 0; i < N; ++i)
-        std::cin >> v[i];
-        
-    std::sort(v.begin(), v.end());
+    std::map<int, int> m;
+    for(int i = 0; i < N; ++i) {
+        std::cin >> num;
+        m[num]++;
+    }
     
     std::cin >> N;
     for(int i = 0; i < N; ++i) {
-        count = 0;
         std::cin >> num;
-        auto p = std::lower_bound(v.begin(), v.end(), num);
-        for(; p != v.end(); ++p) {
-            if(*p != num)
-                break;
-            count++;
-        }
-        std::cout << count << ' ';
-    }
-    
+        std::cout << m[num] << ' ';
+    }   
 }
